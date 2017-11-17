4d477e764e0e4abe3cd84b7cf5764689e1b9f6c5
Ashish Jain (NUGET) <asja@microsoft.com>
2017-11-16 17:16:33 -0800

    Added telemetry event data

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Actions/UIActionEngine.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Actions/UIActionEngine.cs
@@ -151,16 +151,6 @@ namespace NuGet.PackageManagement.UI
             if (!string.IsNullOrEmpty(backupPath))
             {
                 var htmlLogFile = GenerateUpgradeReport(nuGetProject, backupPath, upgradeInformationWindowModel);
-                /*Process process = null;
-                try
-                {
-                    process = Process.Start(htmlLogFile);
-                }
-                catch { }
-                if (process == null)
-                {
-                    OpenUrlInInternalWebBrowser(htmlLogFile);
-                }*/
 
                 OpenUrlInInternalWebBrowser(htmlLogFile);
             }
