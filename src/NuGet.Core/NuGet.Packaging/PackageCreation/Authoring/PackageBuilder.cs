1e5b645465063e612b1ba4c1b984e4a4cb3e9af0
ragrawal <ragrawal@microsoft.com>
2017-11-28 18:02:33 -0800

    stop escaping paths

--- src/NuGet.Core/NuGet.Packaging/PackageCreation/Authoring/PackageBuilder.cs
+++ src/NuGet.Core/NuGet.Packaging/PackageCreation/Authoring/PackageBuilder.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -734,8 +734,7 @@ namespace NuGet.Packaging
         internal static string CreatePartEntryName(string path)
         {
             // Only the segments between the path separators should be escaped
-            var segments = path.Split(new[] { '/', '\\', Path.DirectorySeparatorChar }, StringSplitOptions.None)
-                               .Select(Uri.EscapeDataString);
+            var segments = path.Split(new[] { '/', '\\', Path.DirectorySeparatorChar }, StringSplitOptions.None);
 
             var escapedPath = String.Join("/", segments);
 
